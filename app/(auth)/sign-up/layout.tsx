fb43c017669933f93cbbc7101c227e7c3af5d79c
Aj-1872 <ajayjodedra.com>
2024-07-08 15:50:13 -0400

    initializing  clerk root  and auth (onboarding ,  sing-in , sign-up(Layout) )

---
+++ app/(auth)/sign-up/layout.tsx
@@ -0,0 +1,36 @@
+
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Roboto } from "next/font/google";
+import "../globals.css"
+
+interface propType {
+    children : React.ReactNode;
+}
+
+export const metadata = {
+    title: "threads",
+    discription : "creating threads using NEXT 13"
+}
+
+const inter = Inter({ subsets: ["latin"], weight :"100" });
+
+
+
+export default function ReactLayout ({children} : propType){
+
+    return (
+        <ClerkProvider>
+
+           <html lang="en">
+            <body className={`${inter.className} bg-dark-1`}>
+                {children}
+            </body>
+            
+           </html>
+
+        </ClerkProvider>
+    )
+
+}
+
